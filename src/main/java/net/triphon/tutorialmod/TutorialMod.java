172dd0c41ae7cf8ed0b3cdbe07ab15eb09da7529
BuildTools <unconfigured@null.spigotmc.org>
2022-07-15 05:12:17 +0200

    added a little comment

--- src/main/java/net/triphon/tutorialmod/TutorialMod.java
+++ src/main/java/net/triphon/tutorialmod/TutorialMod.java
@@ -3,7 +3,7 @@ package net.triphon.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//Very important comment :)
+//Very important comment :)(:
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
